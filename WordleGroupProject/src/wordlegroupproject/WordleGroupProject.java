126428cf147db556f10ffb33fd218a42bac31b13
koimcf2005 <47841930+koimcf2005@users.noreply.github.com>
2024-01-30 11:10:44 -0500

    Init

---
+++ WordleGroupProject/src/wordlegroupproject/WordleGroupProject.java
@@ -0,0 +1,16 @@
+package wordlegroupproject;
+
+/**
+ * This is the main class
+ * 
+ * @author Koi McFarland
+ * @author Andrew Burbage
+ * @author Kayla Dixon
+ * @author Pasquale Concilio
+ */
+public class WordleGroupProject {
+    public static void main(String[] args) {
+        
+    }
+    
+}
