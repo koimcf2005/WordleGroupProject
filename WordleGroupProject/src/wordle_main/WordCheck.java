59831fcd017f77b7c49847be68d332a5b0f37432
AndrewBurbage <158192295+AndrewBurbage@users.noreply.github.com>
2024-04-16 10:13:24 -0400

    actual delete

--- WordleGroupProject/src/wordle_main/WordCheck.java
+++ WordleGroupProject/src/wordle_main/WordCheck.java
@@ -6,7 +6,9 @@ import wordle_gui.Window;
  * @author andre
  */
 public class WordCheck {
-    // Object variables
+    
+   private static boolean hasWon = false;
+    
     private static Window mWindow = WordleGroupProject.mWindow;
 
     public static void checkGuess(String selectedWord, String guess, int guessCount) {
@@ -20,11 +22,18 @@ public class WordCheck {
                 if (lowGuess.charAt(i) == selectedWord.charAt(i)) {
                     mWindow.getGrid().getBox(guessCount, i).setAsCorrectLetter();   
                 }
-                System.out.println("");
             } else {
                 mWindow.getGrid().getBox(guessCount, i).setAsIncorrectLetter();
                 mWindow.getKeyboard().getButtonByText(Character.toString(guess.charAt(i))).setAsGrey();   
             }
         }
+        if(lowGuess.compareTo(selectedWord)== 0){
+            hasWon = true;
+        }
     }
+    
+    public boolean getWin(){
+        return hasWon;
+    }
+    
 }
